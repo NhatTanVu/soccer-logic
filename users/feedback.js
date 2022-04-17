e4e820d70df8c59b263aeccc7d28e9199d505f1f
Nhat Tan Vu <onchua2006@gmail.com>
2022-04-16 21:55:25 -0700

    Update feedback success message.

--- users/feedback.js
+++ users/feedback.js
@@ -4,14 +4,14 @@ function onSubmit() {
         var message = "Your feedback was sent. Thanks for contacting us!";
 
         document.querySelector(".dialog > .title").innerHTML = "Feedback";
-        document.querySelector(".dialog > .content").innerHTML = message + ". Redirect in " + (timeLeft / 1000) + "s...";
+        document.querySelector(".dialog > .content").innerHTML = message + " Redirect in " + (timeLeft / 1000) + "s...";
         document.querySelector(".dialog").style.display = "flex";
         document.querySelector(".dialog + .overlay").style.display = "block";
 
         window.setInterval(function () {
             if (timeLeft > 0) {
                 timeLeft -= 1000;
-                document.querySelector(".dialog > .content").innerHTML = message + ". Redirect in " + (timeLeft / 1000) + "s...";
+                document.querySelector(".dialog > .content").innerHTML = message + " Redirect in " + (timeLeft / 1000) + "s...";
             }
             else {
                 window.location.replace("../index.html");
