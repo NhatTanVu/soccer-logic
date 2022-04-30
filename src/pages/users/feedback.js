5f0f0e771bc1f8402d04102d4f7912b24a415ee3
Nhat Tan Vu <onchua2006@gmail.com>
2022-04-30 15:02:40 -0700

    Add CSS styles for Register and Feedback page in NextJS.

--- src/pages/users/feedback.js
+++ src/pages/users/feedback.js
@@ -21,6 +21,40 @@ export default function SignIn() {
             <header className="hero">
                 <h1>Feedback</h1>
             </header>
+            <section className={`${localStyles["flex"]} flex`}>
+                <form className="card" onsubmit="onSubmit.call(this, event);">
+                    <div className={`${localStyles["vertical-flex"]} vertical-flex`}>
+                        <div className={`${localStyles["flex"]} flex`}>
+                            <div className={`${localStyles["flex"]} flex`}>
+                                <label className="required" for="email">Email Address</label>
+                                <input type="email" id="email" required />
+                            </div>
+                            <div className={`${localStyles["flex"]} flex`}>
+                                <label for="first_name">First Name</label>
+                                <input type="text" id="first_name" />
+                            </div>
+                            <div className={`${localStyles["flex"]} flex`}>
+                                <label for="last_name">Last Name</label>
+                                <input type="text" id="last_name" />
+                            </div>
+                        </div>
+                        <hr />
+                        <div className={`${localStyles["grid"]} grid`}>
+                            <label for="rate" className={`${localStyles["flex"]} flex`}>
+                                <span>Rate the overall experience&nbsp;</span><span>(0=poor &#8594; 10=great)</span>
+                            </label>
+                            <input type="range" value="5" step="1" min="0" max="10" id="rate" />
+                            <label for="commBox">Tell us more about your experience!</label>
+                            <textarea name="custExp" id="commBox" rows="5"></textarea>
+                        </div>
+                        <div id={localStyles["summary_message"]} className="highlight">&nbsp;</div>
+                    </div>
+                    <div>
+                        <button id={localStyles["submit_button"]} className="form-button" type="submit">Submit</button>
+                        <button id="cancel_button" className="form-button" onclick="onCancel.call(this, event);">Cancel</button>
+                    </div>
+                </form>
+            </section>
         </Layout>
     )
 }
--- src/pages/users/register.js
+++ src/pages/users/register.js
@@ -21,6 +21,60 @@ export default function SignIn() {
             <header className="hero">
                 <h1>Register</h1>
             </header>
+            <section className={`flex ${localStyles["flex"]}`}>
+                <form id={localStyles["register_form"]} className="card" onsubmit="onSubmit.call(this, event);">
+                    <div>
+                        <div className="flex">
+                            <div className="left">
+                                <div className={`flex ${localStyles["row"]} form-group`}>
+                                    <label className="required" for="email">Email Address</label>
+                                    <input type="email" id="email" required />
+                                </div>
+                                <div className={`flex ${localStyles["row"]} form-group`}>
+                                    <label className="required" for="password">Password</label>
+                                    <input type="password" id="password" required />
+                                </div>
+                                <div className={`flex ${localStyles["row"]}`}>
+                                    <label for="first_name">First Name</label>
+                                    <input type="text" id="first_name" />
+                                </div>
+                            </div>
+                            <div className="right">
+                                <div className={`flex ${localStyles["row"]} form-group`}>
+                                    <label for="last_name">Last Name</label>
+                                    <input type="text" id="last_name" />
+                                </div>
+                                <div className={`flex ${localStyles["row"]} form-group`}>
+                                    <label>Gender</label>
+                                    <div id={localStyles["gender-group"]} className="flex">
+                                        <div className="flex">
+                                            <input type="radio" id="gender_male" name="gender" />
+                                            <span for="gender_male">Male</span>
+                                        </div>
+                                        <div className="flex">
+                                            <input type="radio" id="gender_female" name="gender" />
+                                            <span for="gender_female">Female</span>
+                                        </div>
+                                        <div className="flex">
+                                            <input type="radio" id="gender_unspecified" name="gender" />
+                                            <span for="gender_unspecified">Unspecified</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={`flex ${localStyles["row"]}`}>
+                                    <label for="dob">Date of Birth</label>
+                                    <input type="date" id="dob" />
+                                </div>
+                            </div>
+                        </div>
+                        <div id={localStyles["summary_message"]} className="highlight">&nbsp;</div>
+                    </div>
+                    <div>
+                        <button id={localStyles["submit_button"]} className="form-button" type="submit"><span>Register</span><span className="hideMed hideSmall"> &#8594; Sign In</span></button>
+                        <button id="cancel_button" className="form-button" onclick="onCancel.call(this, event);">Cancel</button>
+                    </div>
+                </form>
+            </section>
         </Layout>
     )
 }
